b76bb9c91bcadfcc0822acd761d2a2ce2536ab09
mihl20 <mihl@hotmail.co.uk>
2021-10-03 16:19:45 +0100

    First commit saving openeing pages and style sheets

---
+++ app.js
@@ -0,0 +1,23 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/portfolio", function(req, res){
+  res.render("portfolio");
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
