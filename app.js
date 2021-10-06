a379ef1fd9631545ba5593145266d13d4400b2a7
mihl20 <mihl@hotmail.co.uk>
2021-10-06 16:34:08 +0100

    Add full website and javascript files

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -17,6 +16,46 @@ app.get("/", function(req, res){
 app.get("/portfolio", function(req, res){
   res.render("portfolio");
 });
+
+app.get("/pets", function(req, res){
+  res.render("pets");
+});
+
+app.get("/weddings", function(req, res){
+  res.render("weddings");
+});
+
+app.get("/modelling", function(req, res){
+  res.render("modelling");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.get("/pricing", function(req, res){
+  res.render("pricing");
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact");
+});
+
+app.post("/", function(req, res){
+
+  var name = req.body.fname;
+  var email = req.body.email;
+  var message = req.body.Message;
+
+  console.log(name, email, message);
+});
+
+function submit_form() {
+  document.form1.submit();
+  document.form1.reset();
+}
+
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
---
+++ public/javascript/inside.js
@@ -0,0 +1,7 @@
+
+var delayInMilliseconds = 1000;
+
+function sendAndReset(){
+document.getElementById("myForm").submit();
+document.getElementById("myForm").reset();
+}
