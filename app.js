185c8f43b35672400c888c90abc793e219cc5916
mihl20 <mihl@hotmail.co.uk>
2021-10-06 19:57:15 +0100

    Tidy up Files

--- app.js
+++ app.js
@@ -6,42 +6,44 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/portfolio", function(req, res){
+app.get("/portfolio", function(req, res) {
   res.render("portfolio");
 });
 
-app.get("/pets", function(req, res){
+app.get("/pets", function(req, res) {
   res.render("pets");
 });
 
-app.get("/weddings", function(req, res){
+app.get("/weddings", function(req, res) {
   res.render("weddings");
 });
 
-app.get("/modelling", function(req, res){
+app.get("/modelling", function(req, res) {
   res.render("modelling");
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.get("/pricing", function(req, res){
+app.get("/pricing", function(req, res) {
   res.render("pricing");
 });
 
-app.get("/contact", function(req, res){
+app.get("/contact", function(req, res) {
   res.render("contact");
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
 
   var name = req.body.fname;
   var email = req.body.email;
@@ -57,6 +59,6 @@ function submit_form() {
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
